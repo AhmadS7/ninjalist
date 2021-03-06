5b77dfc1608cc5cb8c69bf1e1e03582a319277ee
Ahmad <myvirtualrabbit15@gmail.com>
2021-03-06 08:13:32 +0600

    ninja-list

--- pages/about.js
+++ pages/about.js
@@ -1,34 +1,27 @@
 import Head from 'next/head';
-import Footer from '../comps/Footer';
-import Navbar from '../comps/Navbar';
-import styles from '../styles/Home.module.css';
-import Link from 'next/link';
 
-export default function Home() {
+const About = () => {
   return (
     <>
       <Head>
-        <title>Ninja List | Home</title>
+        <title>Ninja List | About</title>
         <meta name="keywords" content="ninjas" />
       </Head>
       <div>
-        <h1 className={styles.title}>HomePage</h1>
-        <p className={styles.text}>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Id pariatur
-          eaque ullam nihil consectetur quam maiores debitis voluptatem eos
-          numquam !
-        </p>
-
-        <p className={styles.text}>
+        <h1>About</h1>
+        <p>
           Lorem ipsum dolor sit amet, consectetur adipisicing elit. Id pariatur
           eaque ullam nihil consectetur quam maiores debitis voluptatem eos
           numquam!
         </p>
-
-        <Link href="/ninjas">
-          <a className={styles.btn}>See Ninja Listing</a>
-        </Link>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Id pariatur
+          eaque ullam nihil consectetur quam maiores debitis voluptatem eos
+          numquam!
+        </p>
       </div>
     </>
   );
-}
+};
+
+export default About;
